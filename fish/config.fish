a2caa1512dbe7ded8e59ddb1e74c06004b195372
Jonathan Barrett <j.e.barrett@gmail.com>
2021-03-08 19:42:26 +0000

    sets editor to nvim in fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,10 @@
 # initialise starship prompt
 starship init fish | source
 
+# set editor to neovim
+set -x EDITOR nvim
+set -x VISUAL nvim
+
 # initialise chruby
 source (brew --prefix)/share/chruby/chruby.fish
 source (brew --prefix)/share/chruby/auto.fish
