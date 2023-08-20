233e204109bf234f69dace6f7583d2f6f5c3dc45
Jonathan Barrett <jonathan@relativesanity.com>
2023-08-20 16:04:22 +0100

    adds lua config for nvim

---
+++ nvim/lua/custom/plugins.lua
@@ -0,0 +1,23 @@
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+	{
+		'nvim-telescope/telescope.nvim',
+		dependencies = { 'nvim-lua/plenary.nvim' }
+	},
+	{ 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
+	{ 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' },
+}
+
+require('lazy').setup(plugins, opts)
