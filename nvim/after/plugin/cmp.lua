88eea5b7acb808a1cf33558b9689af788eaf578b
Jonathan Barrett <jonathan@relativesanity.com>
2023-08-22 08:00:02 +0100

    tidies up completion and snippets

--- nvim/after/plugin/cmp.lua
+++ nvim/after/plugin/cmp.lua
@@ -1,8 +1,8 @@
-local cmp = require 'cmp'
 local luasnip = require 'luasnip'
 require('luasnip.loaders.from_vscode').lazy_load()
 luasnip.config.setup {}
 
+local cmp = require 'cmp'
 cmp.setup {
   snippet = {
     expand = function(args)
@@ -14,7 +14,6 @@ cmp.setup {
     ['<C-p>'] = cmp.mapping.select_prev_item(),
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete {},
     ['<CR>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
@@ -39,7 +38,6 @@ cmp.setup {
     end, { 'i', 's' }),
   },
   sources = {
-    -- { name = 'nvim_lsp' },
     { name = 'luasnip' },
   },
 }
