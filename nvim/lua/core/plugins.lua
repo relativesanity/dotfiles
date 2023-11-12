55f857f4157521b0db07b222472d0f067084ec34
Jonathan Barrett <jonathan@relativesanity.com>
2023-11-12 14:56:51 +0000

    consistent quoting

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -1,20 +1,20 @@
 local plugins = {
   -- catpuccin for theme
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
 
   -- devicons for lualine and nvim-tree
-  { "nvim-tree/nvim-web-devicons" },
+  { 'nvim-tree/nvim-web-devicons' },
   -- statusline awesomeness
-  { "nvim-lualine/lualine.nvim" },
+  { 'nvim-lualine/lualine.nvim' },
   -- filetree
-  { "nvim-tree/nvim-tree.lua" },
+  { 'nvim-tree/nvim-tree.lua' },
   -- telescope for file navmgation
   {
     'nvim-telescope/telescope.nvim', tag = '0.1.4',
     dependencies = { 'nvim-lua/plenary.nvim' }
   },
   -- treesitter for parsing and syntax highlighting
-  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" }
+  { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' }
 }
 
 
@@ -22,16 +22,16 @@ local plugins = {
 
 
 -- Ensure lazy is installed
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
-    "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
+    'git', 'clone', '--filter=blob:none', 'https://github.com/folke/lazy.nvim.git', '--branch=stable',
     lazypath,
   })
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- lazy setup with plugins and options defined above
-require("lazy").setup(plugins)
+require('lazy').setup(plugins)
 -- load config for plugins
 require('core.config')
