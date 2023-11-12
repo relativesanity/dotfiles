3d113f114ec3fc57b4f01f062f060d5302c94b2c
Jonathan Barrett <jonathan@relativesanity.com>
2023-11-12 21:49:00 +0000

    adds gitsigns helpers

--- nvim/lua/core/config/gitsigns.lua
+++ nvim/lua/core/config/gitsigns.lua
@@ -1,1 +1,16 @@
-require('gitsigns').setup()
+require('gitsigns').setup({
+  on_attach = function(bufnr)
+    local gs = package.loaded.gitsigns
+
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    -- Actions
+    map('n', '<leader>gb', function() gs.blame_line{full=true} end)
+    map('n', '<leader>tb', gs.toggle_current_line_blame)
+    map('n', '<leader>td', gs.toggle_deleted)
+  end
+})
