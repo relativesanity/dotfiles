e71080781b3c1f3a1346413d404041ab86bd42d4
Jonathan Barrett <jonathan@relativesanity.com>
2023-11-14 21:23:04 +0000

    adds some things to try in telescope

--- nvim/lua/core/config/telescope.lua
+++ nvim/lua/core/config/telescope.lua
@@ -2,8 +2,11 @@ local builtin = require('telescope.builtin')
 require('telescope').load_extension('fzf')
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files)
-vim.keymap.set('n', '<leader>fg', builtin.live_grep)
+vim.keymap.set('n', '<leader>fs', builtin.live_grep)
 vim.keymap.set('n', '<leader>fb', builtin.buffers)
 vim.keymap.set('n', '<leader>fh', builtin.help_tags)
 vim.keymap.set('n', '<leader>fo', builtin.oldfiles)
 vim.keymap.set('n', '<leader>fc', builtin.commands)
+
+vim.keymap.set('n', '<leader>fgc', builtin.git_commits)
+vim.keymap.set('n', '<leader>fch', builtin.command_history)
