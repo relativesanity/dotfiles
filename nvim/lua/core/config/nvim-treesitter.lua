e7ba9ae7b6ee6181daf1f40ddaaced17eef476c4
Jonathan Barrett <jonathan@relativesanity.com>
2023-11-14 13:00:56 +0000

    adds zsh highlighting

--- nvim/lua/core/config/nvim-treesitter.lua
+++ nvim/lua/core/config/nvim-treesitter.lua
@@ -9,3 +9,18 @@ require('nvim-treesitter.configs').setup({
 -- workaround dot indentation issue
 -- see here: https://github.com/nvim-treesitter/nvim-treesitter/issues/3363
 vim.cmd('autocmd FileType ruby setlocal indentkeys-=.')
+
+-- make zsh files recognized as sh for bash-ls & treesitter
+-- credit to https://nanotipsforvim.prose.sh/treesitter-and-lsp-support-for-zsh
+vim.filetype.add {
+  extension = {
+    zsh = 'sh',
+    sh = 'sh', -- force sh-files with zsh-shebang to still get sh as filetype
+  },
+  filename = {
+    ['.zshrc'] = 'sh',
+    ['.zshenv'] = 'sh',
+    ['zprofile'] = 'sh', -- add this as it's how I store my dotfile
+    ['zshrc'] = 'sh',    -- add this as it's how I store my dotfile
+  },
+}
