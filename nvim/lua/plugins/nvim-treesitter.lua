25541b4e9a3468d3b0d47c4952811f9dd833a1e8
Jonathan Barrett <jonathan@relativesanity.com>
2023-12-29 12:31:19 +0000

    no need for ensuring installed when auto install is in place

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -3,7 +3,6 @@ return {
   build = ':TSUpdate',
   config = function()
     require('nvim-treesitter.configs').setup({
-      ensure_installed = { 'lua', 'ruby', 'html', 'css', 'vim', 'vimdoc' },
       sync_install = false,
       auto_install = true,
       indent = { enable = true, },
