bdfc04cc086b7594e0befcc5b1266ce8508388e3
Jonathan Barrett <jonathan@relativesanity.com>
2023-12-29 14:56:53 +0000

    adds completions back in

---
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -0,0 +1,76 @@
+return {
+  'hrsh7th/nvim-cmp',
+  dependencies = {
+    { 'hrsh7th/cmp-buffer' },
+    { 'hrsh7th/cmp-path' },
+    { 'hrsh7th/cmp-cmdline' },
+    { 'L3MON4D3/LuaSnip' },
+    { 'saadparwaiz1/cmp_luasnip' },
+    { 'rafamadriz/friendly-snippets' },
+  },
+  config = function()
+    local luasnip = require('luasnip')
+    require('luasnip/loaders/from_vscode').lazy_load()
+    luasnip.filetype_extend('ruby', {'rails'})
+
+    local cmp = require('cmp')
+    cmp.setup({
+      sources = {
+        { name = 'luasnip' },
+        { name = 'nvim_lsp' },
+        { name = 'buffer' },
+        { name = 'path' },
+      },
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end
+      },
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
+      mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<CR>'] = cmp.mapping.confirm({ select = false }),
+
+        -- enable "Super-Tab" mapping as per
+        -- https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#super-tab-like-mapping
+        ['<Tab>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_locally_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            fallback()
+          end
+        end, { 'i', 's' }),
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.locally_jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { 'i', 's' }),
+      }),
+    })
+
+    cmp.setup.cmdline({'/', '?'}, {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = 'buffer' }
+      }
+    })
+
+    cmp.setup.cmdline(':', {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = 'path' },
+        { name = 'cmdline' }
+      })
+    })
+  end
+}
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,9 +1,7 @@
 return {
   {
     'nvim-telescope/telescope.nvim',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-    },
+    dependencies = { 'nvim-lua/plenary.nvim', },
     keys = {
       { '<leader>ff', '<cmd>Telescope find_files<CR>', desc = '(f)ind (f)iles' },
       { '<leader>fs', '<cmd>Telescope live_grep<CR>', desc = '(f)ind by (s)earch' },
@@ -11,7 +9,6 @@ return {
       { '<leader>fh', '<cmd>Telescope help_tags<CR>', desc = '(f)ind in (h)elp' },
       { '<leader>fo', '<cmd>Telescope oldfiles<CR>', desc = '(f)ind in (o)ld files' },
       { '<leader>fc', '<cmd>Telescope commands<CR>', desc = '(f)ind (c)ommands' },
-
       { '<leader>fg', '<cmd>Telescope git_commits<CR>', desc = '(f)ind (g)it commits' },
     },
   },
