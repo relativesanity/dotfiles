7e8412680446ff963d469b798b30fa6f097c7576
Jonathan Barrett <jonathan@relativesanity.com>
2023-12-29 12:45:00 +0000

    tidies up telescope extension config

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -3,8 +3,6 @@ return {
     'nvim-telescope/telescope.nvim',
     dependencies = {
       'nvim-lua/plenary.nvim',
-      'nvim-telescope/telescope-ui-select.nvim',
-      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     },
     keys = {
       { '<leader>ff', '<cmd>Telescope find_files<CR>', desc = '(f)ind (f)iles' },
@@ -16,17 +14,25 @@ return {
 
       { '<leader>fg', '<cmd>Telescope git_commits<CR>', desc = '(f)ind (g)it commits' },
     },
+  },
+  {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    build = 'make',
     config = function()
       require('telescope').load_extension('fzf')
-
-      require('telescope').setup({
-        extensions = {
-          ['ui-select'] = {
-            require('telescope.themes').get_dropdown {}
-          }
-        }
-      })
-      require('telescope').load_extension('ui-select')
     end
   },
+  {
+    'nvim-telescope/telescope-ui-select.nvim',
+    config = function()
+      require("telescope").setup {
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({})
+          }
+        }
+      }
+      require("telescope").load_extension("ui-select")
+    end
+  }
 }
