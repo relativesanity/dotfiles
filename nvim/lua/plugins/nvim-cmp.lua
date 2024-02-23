3e3ba9f9e3afa9deff68dcc4d971ec493b53283e
Jonathan Barrett <jonathan@relativesanity.com>
2024-02-23 21:42:37 +0000

    adds copilot back in

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -17,6 +17,7 @@ return {
     local cmp = require('cmp')
     cmp.setup({
       sources = {
+        { name = "copilot" },
         { name = 'luasnip' },
         { name = 'nvim_lsp' },
         { name = 'buffer' },
