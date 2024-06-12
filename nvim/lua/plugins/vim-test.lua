f1187b8c95970f006f463d524326084c2d3791b4
Jonathan Barrett <jonathan@relativesanity.com>
2024-06-12 20:26:25 +0100

    removes copilot

--- nvim/lua/plugins/vim-test.lua
+++ nvim/lua/plugins/vim-test.lua
@@ -4,6 +4,7 @@ return {
     vim.keymap.set("n", "<leader>rn", ":TestNearest<CR>", { desc = "(R)un (n)earest test" })
     vim.keymap.set("n", "<leader>rf", ":TestFile<CR>", { desc = "(R)un test (f)ile" })
     vim.keymap.set("n", "<leader>ra", ":TestSuite<CR>", { desc = "(R)un (a)ll tests" })
+    vim.keymap.set("n", "<leader>rl", ":TestLast<CR>", { desc = "(R)un (l)ast test" })
 
     vim.g["test#strategy"] = "vimux"
   end
