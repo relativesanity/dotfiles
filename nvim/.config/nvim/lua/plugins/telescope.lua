be8850cee3e694a6c57e621802603762cf137979
Jonathan Barrett <jonathan@relativesanity.com>
2024-06-15 09:36:12 +0100

    ensures neovim can find dotfiles for editing

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -3,14 +3,33 @@ return {
     'nvim-telescope/telescope.nvim',
     dependencies = { 'nvim-lua/plenary.nvim', },
     keys = {
-      { '<leader>ff', '<cmd>Telescope find_files<CR>', desc = '(f)ind (f)iles' },
-      { '<leader>fs', '<cmd>Telescope live_grep<CR>', desc = '(f)ind by (s)earch' },
+      { '<leader>ff', '<cmd>Telescope find_files hidden=true<CR>', desc = '(f)ind (f)iles' },
+      { '<leader>fs', '<cmd>Telescope live_grep hidden=true<CR>', desc = '(f)ind by (s)earch' },
       { '<leader>fb', '<cmd>Telescope buffers<CR>', desc = '(f)ind open (b)uffers' },
       { '<leader>fh', '<cmd>Telescope help_tags<CR>', desc = '(f)ind in (h)elp' },
-      { '<leader>fo', '<cmd>Telescope oldfiles<CR>', desc = '(f)ind in (o)ld files' },
+      { '<leader>fo', '<cmd>Telescope oldfiles hidden=true<CR>', desc = '(f)ind in (o)ld files' },
       { '<leader>fc', '<cmd>Telescope commands<CR>', desc = '(f)ind (c)ommands' },
       { '<leader>fg', '<cmd>Telescope git_commits<CR>', desc = '(f)ind (g)it commits' },
     },
+    config = function()
+      require("telescope").setup {
+        defaults = {
+          file_ignore_patterns = {
+            ".git/"
+          },
+          vimgrep_arguments = {
+            "rg",
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--smart-case",
+            "--hidden"
+          }
+        }
+      }
+    end
   },
   {
     'nvim-telescope/telescope-fzf-native.nvim',
