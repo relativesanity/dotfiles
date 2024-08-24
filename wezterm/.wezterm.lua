a41d3b7a130270a279d70257235d15275bdf062d
Jonathan Barrett <jonathan@relativesanity.com>
2024-08-24 17:04:37 +0100

    makes tab bar a little smarter

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -4,7 +4,7 @@ local config = wezterm.config_builder()
 
 config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
 config.font_size = 17
-config.enable_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
 config.color_scheme = "Catppuccin Mocha"
 
