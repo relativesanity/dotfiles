ba11ae3016404fede06e53aec763961b6650fb09
Jonathan Barrett <jonathan@relativesanity.com>
2024-09-09 22:43:43 +0100

    fixes screen size and switches option/alt keys

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -10,7 +10,10 @@ config.color_scheme = "Catppuccin Mocha"
 config.window_background_opacity = 0.85
 config.macos_window_background_blur = 100
 
-config.initial_cols = 144
+config.initial_cols = 146
 config.initial_rows = 39
+
+config.send_composed_key_when_left_alt_is_pressed = true
+config.send_composed_key_when_right_alt_is_pressed = false
 
 return config
