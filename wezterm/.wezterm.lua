65335a107a81abb7a1aab0f2f7cf0eddd2fed9fa
Jonathan Barrett <jonathan@relativesanity.com>
2024-10-08 19:08:33 +0100

    uses Cascadia for wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -2,8 +2,8 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
-config.font_size = 17
+config.font = wezterm.font("Cascadia Code NF")
+config.font_size = 18
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
 config.color_scheme = "Catppuccin Mocha"
