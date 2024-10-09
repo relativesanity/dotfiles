4f0730cdc696a34645af99d6d64b8b040e791067
Jonathan Barrett <jonathan@relativesanity.com>
2024-10-09 20:30:42 +0100

    makes font a bit smaller

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.font = wezterm.font("Cascadia Code NF")
-config.font_size = 18
+config.font_size = 17
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
 config.color_scheme = "Catppuccin Mocha"
