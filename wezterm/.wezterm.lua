9049c912027b7befe3aeb677b020fa40f391c2e9
Jonathan Barrett <jonathan@relativesanity.com>
2024-10-10 22:44:58 +0100

    loosens up the line height

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -4,10 +4,11 @@ local config = wezterm.config_builder()
 
 config.font = wezterm.font("Cascadia Code NF")
 config.font_size = 17
+config.line_height = 1.1
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
 config.color_scheme = "Catppuccin Mocha"
-config.window_background_opacity = 0.90
+config.window_background_opacity = 0.97
 config.macos_window_background_blur = 100
 
 config.initial_cols = 140
