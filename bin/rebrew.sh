5fd0940c76e5d40a7a789c7d3dc91db695d7a283
Jonathan Barrett <jonathan@relativesanity.com>
2025-02-17 21:14:31 +0000

    ensures brew is installed and enabled before rebrewing

--- bin/rebrew.sh
+++ bin/rebrew.sh
@@ -1,10 +1,27 @@
 #!/usr/bin/env bash
 
-# Check if we're using homebrew
-if command -c brew >/dev/null 2>&1; then
+# Check if we're dealing with macOS
+if command -v defaults >/dev/null 2>&1; then
+  # Do we have homebrew installed already?
+  if [[ ! -e /opt/homebrew/bin/brew ]]; then
+    echo "Installing homebrew"
+    /usr/bin/env bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  fi
+  echo "Homebrew installed"
+
+  # We now have homebrew installed, so if it's not an available command,
+  # we must configure it for the current session
+  if ! command -v brew >/dev/null 2>&1; then
+    echo "Enabling homebrew"
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+  fi
+  echo "Homebrew enabled"
+
   echo "Updating homebrew"
   brew update
+
   echo "Bundling homebrew"
   brew bundle --file "$HOME"/.dotfiles/Brewfile --cleanup --zap
 fi
+
 echo "Rebrew complete"
