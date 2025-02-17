5d5912048da546f519feeaeaa4892aa145ff54e5
Jonathan Barrett <jonathan@relativesanity.com>
2025-02-17 22:48:20 +0000

    moves common defaults to m.sh

--- bin/m.sh
+++ bin/m.sh
@@ -4,4 +4,17 @@ if command -v m >/dev/null 2>&1; then
   m dock prune
   m dock autohide YES
   m dock showdelay 0.0
+  # fast dock hide and show
+  # to reset:
+  #   defaults delete com.apple.dock autohide-delay
+  #   defaults write com.apple.dock autohide-time-modifier -float "0.5"
+  #   killall Dock
+  defaults write com.apple.dock autohide-delay -float 0
+  defaults write com.apple.dock autohide-time-modifier -int 0
+  killall Dock
+
+  # disable window opening animations
+  # to reset:
+  #   defaults write -g NSAutomaticWindowAnimationsEnabled -bool true
+  defaults write -g NSAutomaticWindowAnimationsEnabled -bool false
 fi
--- sh/.aliases.sh
+++ sh/.aliases.sh
@@ -14,22 +14,3 @@ DOTFILES_PATH="$HOME/.dotfiles/"
 alias dotfiles='cd $DOTFILES_PATH'
 alias restow='$HOME/.dotfiles/bin/restow.sh'
 alias rebrew='$HOME/.dotfiles/bin/rebrew.sh'
-
-if command -v defaults >/dev/null 2>&1; then
-  # fast dock hide and show
-  # to reset:
-  #   defaults delete com.apple.dock autohide-delay
-  #   defaults write com.apple.dock autohide-time-modifier -float "0.5"
-  #   killall Dock
-  alias dockf='defaults write com.apple.dock autohide-delay -float 0; defaults write com.apple.dock autohide-time-modifier -int 0; killall Dock'
-
-  # drag window with ctrl and cmd
-  # to reset:
-  #   defaults write -g NSWindowShouldDragOnGesture -bool false
-  alias grabmove='defaults write -g NSWindowShouldDragOnGesture -bool true'
-
-  # disable window opening animations
-  # to reset:
-  #   defaults write -g NSAutomaticWindowAnimationsEnabled -bool true
-  alias disableanim='defaults write -g NSAutomaticWindowAnimationsEnabled -bool false'
-fi
