606fa4c5b4ca718e9508d1b9b751d1e49e485d0c
Jonathan Barrett <jonathan@relativesanity.com>
2025-03-11 16:22:35 +0000

    adds neo tree config

---
+++ neovim/.config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,21 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = false,
+      },
+    },
+  },
+  keys = {
+    { '<leader>t', '<cmd>Neotree toggle<cr>', desc = '(T)reeview toggle' }
+  },
+}
