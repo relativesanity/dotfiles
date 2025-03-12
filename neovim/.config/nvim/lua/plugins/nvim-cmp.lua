b0d4fc451cc0a226c82b25d0ce92439adbc5ea67
Jonathan Barrett <jonathan@relativesanity.com>
2025-03-12 18:23:21 +0000

    adds lsp and completions

--- neovim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ neovim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -8,6 +8,7 @@ return {
     { 'hrsh7th/cmp-buffer' },
     { 'hrsh7th/cmp-path' },
     { 'hrsh7th/cmp-cmdline' },
+    { 'hrsh7th/cmp-nvim-lsp' },
   },
 
   config = function()
@@ -18,6 +19,7 @@ return {
     local cmp = require('cmp')
     cmp.setup({
       sources = {
+        { name = 'nvim_lsp' },
         { name = 'luasnip' },
         { name = 'buffer' },
         { name = 'path' },
