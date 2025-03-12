a7f18ea5b48fab0a62465a56617b717eca03117b
Jonathan Barrett <jonathan@relativesanity.com>
2025-03-12 12:53:02 +0000

    removes dead code

--- neovim/.config/nvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -34,18 +34,5 @@ return {
         require('telescope').load_extension('fzf')
       end
     },
-    -- {
-    --   'nvim-telescope/telescope-ui-select.nvim',
-    --   config = function()
-    --     require("telescope").setup {
-    --       extensions = {
-    --         ["ui-select"] = {
-    --           require("telescope.themes").get_dropdown({})
-    --         }
-    --       }
-    --     }
-    --     require("telescope").load_extension("ui-select")
-    --   end
-    -- },
   }
 }
