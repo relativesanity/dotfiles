ed8135ff8fe0a1e8dff222f9b644830d27848677
Jonathan Barrett <jonathan@relativesanity.com>
2025-03-12 18:33:03 +0000

    adds null-ls to manage formatting and linting

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -42,5 +42,19 @@ return {
       })
     end,
     dependencies = { 'williamboman/mason-lspconfig.nvim' },
+  },
+  {
+    'nvimtools/none-ls.nvim',
+    config = function()
+      local null_ls = require('null-ls')
+
+      null_ls.setup({
+        sources = {
+          null_ls.builtins.formatting.stylua,
+          null_ls.builtins.formatting.rubocop,
+          null_ls.builtins.diagnostics.rubocop,
+        },
+      })
+    end,
   }
 }
