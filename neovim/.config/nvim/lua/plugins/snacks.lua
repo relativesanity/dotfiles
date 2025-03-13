eb54a1c2af1d40279024a5f853f51db981cca600
Jonathan Barrett <jonathan@relativesanity.com>
2025-03-13 10:36:21 +0000

    adds snacks for greeter, but removes whitespace plugin as it conflicts

---
+++ neovim/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,16 @@
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  opts = {
+    dashboard = {
+      preset = {
+        header = "Neovim"
+      },
+      sections = {
+        { section = "header" },
+        { section = "startup" },
+      }
+    }
+  }
+}
