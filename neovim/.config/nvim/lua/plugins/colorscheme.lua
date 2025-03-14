64fb8a6efa490d2fdd70af9c53cd3c945d9a005a
Jonathan Barrett <jonathan@relativesanity.com>
2025-03-14 18:13:36 +0000

    formats with treesitter and updated shiftwidth and tab settings

--- neovim/.config/nvim/lua/plugins/colorscheme.lua
+++ neovim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,11 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	priority = 1000,
-	init = function()
-		vim.cmd.colorscheme "catppuccin"
-	end,
-	opts = {
-		transparent_background = true
-	}
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  init = function()
+    vim.cmd.colorscheme "catppuccin"
+  end,
+  opts = {
+    transparent_background = true
+  }
 }
