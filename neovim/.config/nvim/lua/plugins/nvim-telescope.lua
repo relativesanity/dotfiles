2de8274a1875307b4364aa0e34c348328a589e02
Jonathan Barrett <jonathan@relativesanity.com>
2025-03-15 07:50:12 +0000

    tidies up formatting

--- neovim/.config/nvim/lua/plugins/nvim-telescope.lua
+++ neovim/.config/nvim/lua/plugins/nvim-telescope.lua
@@ -18,11 +18,11 @@ return {
     require("telescope").load_extension("fzf")
   end,
   keys = {
-    { "<leader>ff", "<cmd>Telescope find_files hidden=true<cr>", desc = "Find files" },
-    { "<leader>fh", "<cmd>Telescope help_tags<cr>",              desc = "Find help" },
-    { "<leader>fg", "<cmd>Telescope live_grep hidden=true<cr>",  desc = "Find via grep" },
-    { "<leader>fb", "<cmd>Telescope buffers<cr>",                desc = "Find open buffers" },
-    { "<leader>ft", "<cmd>Telescope builtin<cr>",                desc = "Find telescope features" },
+    { "<leader>ff", "<cmd>Telescope find_files hidden=true<cr>",             desc = "Find files" },
     { "<leader>fo", "<cmd>Telescope oldfiles hidden=true only_cwd=true<cr>", desc = "Find recent files" },
+    { "<leader>fh", "<cmd>Telescope help_tags<cr>",                          desc = "Find help" },
+    { "<leader>fg", "<cmd>Telescope live_grep hidden=true<cr>",              desc = "Find via grep" },
+    { "<leader>fb", "<cmd>Telescope buffers<cr>",                            desc = "Find open buffers" },
+    { "<leader>ft", "<cmd>Telescope builtin<cr>",                            desc = "Find telescope features" },
   }
 }
