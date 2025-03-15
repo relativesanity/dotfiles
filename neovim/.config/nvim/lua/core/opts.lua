dbd8a8958b1cbc5d40192d95c74bf6e8e4147c4c
Jonathan Barrett <jonathan@relativesanity.com>
2025-03-15 08:14:57 +0000

    formatting changes

--- neovim/.config/nvim/lua/core/opts.lua
+++ neovim/.config/nvim/lua/core/opts.lua
@@ -1,9 +1,9 @@
-vim.opt.number = true -- display line numbers
-vim.opt.relativenumber = true -- use relative line numbers
-vim.opt.wrap = false -- don't wrap text
+vim.opt.number = true             -- display line numbers
+vim.opt.relativenumber = true     -- use relative line numbers
+vim.opt.wrap = false              -- don't wrap text
 
-vim.opt.expandtab = true -- spaces, not tabs
-vim.opt.shiftwidth = 2 -- indentation is 2 spaces
-vim.opt.tabstop = 2 -- tabs are two spaces
+vim.opt.expandtab = true          -- spaces, not tabs
+vim.opt.shiftwidth = 2            -- indentation is 2 spaces
+vim.opt.tabstop = 2               -- tabs are two spaces
 
 vim.opt.clipboard = "unnamedplus" -- sync register with the system clipboard
