8c90de9d71ca0ad395e13c5417cfdaa7c1d6aa84
Jonathan Barrett <jonathan@relativesanity.com>
2025-03-15 10:52:01 +0000

    adds quickfix navigation

--- neovim/.config/nvim/lua/core/keymaps.lua
+++ neovim/.config/nvim/lua/core/keymaps.lua
@@ -2,3 +2,7 @@
 vim.keymap.set("n", "<leader><leader>x", "<cmd>source %<cr>")
 vim.keymap.set("n", "<leader>x", ":.lua<cr>")
 vim.keymap.set("v", "<leader>x", ":lua")
+
+-- navigate the quickfix
+vim.keymap.set("n", "<leader>cn", "<cmd>cnext<cr>")
+vim.keymap.set("n", "<leader>cp", "<cmd>cprev<cr>")
