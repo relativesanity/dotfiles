a41a47f277f36d963d720fa70c73ca3dc6a92350
Jonathan Barrett <jonathan@relativesanity.com>
2025-03-15 08:15:12 +0000

    adds undo and split options

--- neovim/.config/nvim/lua/core/opts.lua
+++ neovim/.config/nvim/lua/core/opts.lua
@@ -7,3 +7,8 @@ vim.opt.shiftwidth = 2            -- indentation is 2 spaces
 vim.opt.tabstop = 2               -- tabs are two spaces
 
 vim.opt.clipboard = "unnamedplus" -- sync register with the system clipboard
+
+vim.opt.undofile = true           -- store undo history across sessions
+
+vim.opt.splitbelow = true         -- split below instead of above
+vim.opt.splitright = true         -- split right instead of left
