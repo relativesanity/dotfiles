b80ebf979a60465472d8ca2ca1ac458743e38669
Jonathan Barrett <jonathan@relativesanity.com>
2025-03-15 14:53:14 +0000

    adds some basic terminal helpers

--- neovim/.config/nvim/lua/core/keymaps.lua
+++ neovim/.config/nvim/lua/core/keymaps.lua
@@ -6,3 +6,7 @@ vim.keymap.set("v", "<leader>x", ":lua")
 -- navigate the quickfix
 vim.keymap.set("n", "<leader>cn", "<cmd>cnext<cr>")
 vim.keymap.set("n", "<leader>cp", "<cmd>cprev<cr>")
+
+-- pop a terminal
+vim.keymap.set("n", "<leader>t", "<cmd>terminal<cr>")
+vim.keymap.set("t", "<C-t>", "<C-\\><C-n>")
