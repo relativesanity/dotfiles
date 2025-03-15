d42835ac63ac3683018b6de494cf6ec794883aba
Jonathan Barrett <jonathan@relativesanity.com>
2025-03-15 15:06:35 +0000

    adds some sensible metadata to text yank highlight autocmd

--- neovim/.config/nvim/lua/core/autocmds.lua
+++ neovim/.config/nvim/lua/core/autocmds.lua
@@ -1,5 +1,9 @@
 -- highlight yanked text
 vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight text range when yanked",
+  -- this group ensures that if it gets reassigned, it overwrites
+  -- whatever was there initially
+  group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
   callback = function()
     vim.highlight.on_yank()
   end
