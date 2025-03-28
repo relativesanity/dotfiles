d22f71167ae778aa19944b0d5d7ba6846d11c660
Jonathan Barrett <jonathan@relativesanity.com>
2025-03-28 16:22:08 +0000

    adds lazyvim config

--- neovim/.config/nvim/lua/core/init.lua
+++
@@ -1,4 +0,0 @@
-require("core.opts") -- sets sensible editor options
-require("core.lazy") -- bootstraps lazy and installs plugins
-require("core.keymaps") -- sets up general, non-plugin-specific keymaps
-require("core.autocmds") -- defines and loads autocmds
--- neovim/.config/nvim/lua/plugins/mini.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "echasnovski/mini.nvim",
-  version = "*",
-  config = function()
-    require("mini.pairs").setup()
-    require("mini.surround").setup()
-  end
-}
