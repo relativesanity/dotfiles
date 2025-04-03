88038e5344ee4372a0adc71f98edabc0b3ecd346
Jonathan Barrett <jonathan@relativesanity.com>
2025-04-03 21:54:17 +0100

    ensures file finder includes hidden and ignored files

---
+++ neovim/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,17 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      sources = {
+        explorer = {
+          hidden = true, -- show hidden files
+          ignored = true, -- show ignored files
+        },
+        files = {
+          hidden = true, -- show hidden files
+          ignored = true, -- show ignored files
+        },
+      },
+    },
+  },
+}
