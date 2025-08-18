f915278066f30eee9072c26bd9252da7935619cd
Jonathan Barrett <jonathan@relativesanity.com>
2025-08-18 20:26:08 +0100

    inlines home config into main hammerspoon init

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -4,7 +4,51 @@ hs = hs
 meh = { "ctrl", "alt", "shift" }
 hyper = { "ctrl", "alt", "shift", "cmd" }
 
-require("home")
+VAULT_NAME = "jbOSv2"
+
+hs.hotkey.bind(meh, "Q", function()
+	hs.application.launchOrFocus("Day One")
+end)
+
+hs.hotkey.bind(meh, "E", function()
+	hs.application.launchOrFocus("Mail")
+end)
+
+hs.hotkey.bind(meh, "R", function()
+	hs.application.launchOrFocus("Reader")
+end)
+
+hs.hotkey.bind(hyper, "F", function()
+	hs.application.launchOrFocus("FindMy")
+end)
+
+hs.hotkey.bind(meh, "C", function()
+	hs.application.launchOrFocus("Messages")
+end)
+
+hs.hotkey.bind(hyper, "C", function()
+	hs.application.launchOrFocus("Signal")
+end)
+
+hs.hotkey.bind(meh, "V", function()
+	hs.application.launchOrFocus("Claude")
+end)
+
+hs.hotkey.bind(meh, "Z", function()
+	hs.application.launchOrFocus("YouTube")
+end)
+
+hs.hotkey.bind(hyper, "Q", function()
+	hs.application.launchOrFocus("Music")
+end)
+
+hs.hotkey.bind(hyper, "Z", function()
+	hs.application.launchOrFocus("zoom.us")
+end)
+
+hs.hotkey.bind(hyper, "V", function()
+	hs.application.launchOrFocus("Slack")
+end)
 
 hs.hotkey.bind(meh, "W", function()
 	hs.osascript.applescript([[
