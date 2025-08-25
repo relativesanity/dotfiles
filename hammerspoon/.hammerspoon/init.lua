06311da7de754b54c9779b04a9e0e05f32212bbe
Jonathan Barrett <jonathan@relativesanity.com>
2025-08-25 20:34:17 +0100

    Removes redundant config

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -13,23 +13,6 @@ hs.hotkey.bind(hyper, "V", function()
 	hs.eventtap.keyStroke({ "cmd", "shift" }, "4")
 end)
 
-hs.hotkey.bind(meh, "B", function()
-	hs.osascript.applescript([[
-    tell application "Finder"
-        open (path to home folder as text) & ".local:share:wallpaper:"
-    end tell
-  ]])
-	hs.application.launchOrFocus("Finder")
-end)
-
-hs.hotkey.bind(hyper, "B", function()
-	hs.osascript.applescript([[
-    tell application "Finder"
-        set theFile to selection as alias
-        set desktop picture to theFile
-    end tell
-  ]])
-end)
 
 hs.hotkey.bind(meh, "X", function()
 	hs.eventtap.keyStrokes(os.date("%Y-%m-%d"))
