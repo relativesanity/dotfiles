a094b21805ce5315d7cae222fc201c640de17bdf
Jonathan Barrett <jonathan@relativesanity.com>
2025-08-26 10:27:07 +0100

    switches up hammerspoon config to use hyperkeys and bottom left hand row

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -5,22 +5,14 @@ meh = { "ctrl", "alt", "shift" }
 hyper = { "ctrl", "alt", "shift", "cmd" }
 
 -- Utility functions
-hs.hotkey.bind(hyper, "R", function()
+hs.hotkey.bind(hyper, "V", function()
 	hs.reload()
 end)
 
-hs.hotkey.bind(hyper, "V", function()
+hs.hotkey.bind(hyper, "C", function()
 	hs.eventtap.keyStroke({ "cmd", "shift" }, "4")
 end)
 
-
-hs.hotkey.bind(meh, "X", function()
-	hs.eventtap.keyStrokes(os.date("%Y-%m-%d"))
-end)
-
 hs.hotkey.bind(hyper, "X", function()
-	local hour = tonumber(os.date("%I"))
-	local rest = os.date(":%M%p")
-	local timeString = tostring(hour) .. rest
-	hs.eventtap.keyStrokes(string.lower(timeString))
+	hs.eventtap.keyStrokes(os.date("%Y-%m-%d"))
 end)
