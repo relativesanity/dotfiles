26382244336ef8ef23c8aa45ead97be7a5c89363
Jonathan Barrett <jonathan@relativesanity.com>
2025-09-15 09:07:43 +0100

    adds ragtag

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -694,6 +694,7 @@ require('lazy').setup({
         -- ts_ls = {},
         --
 
+        tailwindcss = {},
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
@@ -994,7 +995,7 @@ require('lazy').setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
