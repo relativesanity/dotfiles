10f8dee596a16d63cb32172baba450df85422754
Jonathan Barrett <jonathan@relativesanity.com>
2025-10-14 14:22:19 +0100

    removes numerical workspace indicators

--- sketchybar/.config/sketchybar/plugins/aerospace.sh
+++ sketchybar/.config/sketchybar/plugins/aerospace.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-    sketchybar --set $NAME label.color=0xffcdd6f4
+    sketchybar --set $NAME icon="●" icon.color=0xffcdd6f4
 else
-    sketchybar --set $NAME label.color=0xff6c7086
+    sketchybar --set $NAME icon="○" icon.color=0xff9399b2
 fi
