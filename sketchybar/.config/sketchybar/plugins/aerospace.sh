291374298fd5e55098298b33967329b868db4ace
Jonathan Barrett <jonathan@relativesanity.com>
2025-11-16 08:08:13 +0000

    tidies up sketchybar config and fixes workspace indicator colors

--- sketchybar/.config/sketchybar/plugins/aerospace.sh
+++ sketchybar/.config/sketchybar/plugins/aerospace.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+# Colors match those defined in sketchybarrc
+WORKSPACE_ACTIVE_COLOR=0xffcdd6f4
+WORKSPACE_INACTIVE_COLOR=0xff6c7086
+
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-    sketchybar --set $NAME icon="●" icon.color=0xffcdd6f4
+    sketchybar --set $NAME icon="●" icon.color=$WORKSPACE_ACTIVE_COLOR
 else
-    sketchybar --set $NAME icon="○" icon.color=0xff9399b2
+    sketchybar --set $NAME icon="○" icon.color=$WORKSPACE_INACTIVE_COLOR
 fi
