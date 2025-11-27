3e259009c046ea207122d5c7898e9eaa43ecb30b
Jonathan Barrett <jonathan@relativesanity.com>
2025-11-27 13:54:55 +0000

    displays battery icon in catppuccin blue when on AC power

--- sketchybar/.config/sketchybar/plugins/battery.sh
+++ sketchybar/.config/sketchybar/plugins/battery.sh
@@ -13,19 +13,33 @@ fi
 # Show battery indicator when below 100%
 sketchybar --set $NAME drawing=on
 
-# Determine icon based on battery level
+# Catppuccin colors
+BLUE=0xff89b4fa  # Catppuccin Blue (for AC power)
+
+# Determine icon and color based on battery level
 if [ -n "$CHARGING" ]; then
     ICON="󰂄"  # charging icon
+    COLOR="$BLUE"
 elif [ "$PERCENTAGE" -ge 90 ]; then
     ICON="󰁹"  # full battery
+    COLOR=""
 elif [ "$PERCENTAGE" -ge 75 ]; then
     ICON="󰂂"
+    COLOR=""
 elif [ "$PERCENTAGE" -ge 50 ]; then
     ICON="󰂀"
+    COLOR=""
 elif [ "$PERCENTAGE" -ge 25 ]; then
     ICON="󰁾"
+    COLOR=""
 else
     ICON="󰁺"  # low battery
+    COLOR=""
 fi
 
-sketchybar --set $NAME icon="$ICON" label="${PERCENTAGE}%"
+# Set icon and label with color if on AC power
+if [ -n "$COLOR" ]; then
+    sketchybar --set $NAME icon="$ICON" icon.color="$COLOR" label="${PERCENTAGE}%"
+else
+    sketchybar --set $NAME icon="$ICON" label="${PERCENTAGE}%"
+fi
