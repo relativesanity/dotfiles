f189e4320fb63ee2cc7e236d6d7542bac2af9b96
Jonathan Barrett <jonathan@relativesanity.com>
2025-12-13 13:32:10 +0000

    adds automatic wallpaper switching on dark mode toggle

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -4,6 +4,10 @@ hs = hs
 meh = { "ctrl", "alt", "shift" }
 hyper = { "ctrl", "alt", "shift", "cmd" }
 
+-- Wallpaper configuration
+local wallpaper_light = "10-3-6k.jpg"
+local wallpaper_dark = "apple-black-4k.png"
+
 -- Utility functions
 hs.hotkey.bind(hyper, "V", function()
 	hs.reload()
@@ -27,4 +31,20 @@ end)
 hs.hotkey.bind(hyper, "Z", function()
 	-- Toggle macOS appearance
 	hs.osascript.applescript('tell app "System Events" to tell appearance preferences to set dark mode to not dark mode')
+
+	-- Wait a moment for the system to update, then set wallpaper based on new mode
+	hs.timer.doAfter(0.3, function()
+		local _, isDark = hs.osascript.applescript('tell app "System Events" to tell appearance preferences to get dark mode')
+		local wallpaperFilename = isDark and wallpaper_dark or wallpaper_light
+		local wallpaperPath = os.getenv("HOME") .. "/.local/share/wallpaper/" .. wallpaperFilename
+
+		-- Set wallpaper for all screens
+		hs.osascript.applescript(string.format([[
+			tell application "System Events"
+				tell every desktop
+					set picture to "%s"
+				end tell
+			end tell
+		]], wallpaperPath))
+	end)
 end)
