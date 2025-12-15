4f478935ac1c80228a3b8470c5c9ebfeea4f712b
Jonathan Barrett <jonathan@relativesanity.com>
2025-12-15 08:09:06 +0000

    simplifies mac setup
    m.sh now only prunes the dock, really all I care about. the other
    commands are likely outdated as well

--- bin/m.sh
+++ bin/m.sh
@@ -15,20 +15,20 @@ IFS=$'\n\t'       # Stricter word splitting
 #   - m-cli must be installed (via Homebrew)
 
 if command -v m >/dev/null 2>&1; then
-  m dock prune
-  m dock autohide YES
-  m dock showdelay 0.0
+  m dock --prune
+  # m dock autohide YES
+  # m dock showdelay 0.0
   # fast dock hide and show
   # to reset:
   #   defaults delete com.apple.dock autohide-delay
   #   defaults write com.apple.dock autohide-time-modifier -float "0.5"
   #   killall Dock
-  defaults write com.apple.dock autohide-delay -float 0
-  defaults write com.apple.dock autohide-time-modifier -int 0
-  killall Dock
+  # defaults write com.apple.dock autohide-delay -float 0
+  # defaults write com.apple.dock autohide-time-modifier -int 0
+  # killall Dock
 
   # disable window opening animations
   # to reset:
   #   defaults write -g NSAutomaticWindowAnimationsEnabled -bool true
-  defaults write -g NSAutomaticWindowAnimationsEnabled -bool false
+  # defaults write -g NSAutomaticWindowAnimationsEnabled -bool false
 fi
