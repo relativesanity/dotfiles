c89122741e30c04d67b24b0c2c1127747b398442
Jonathan Barrett <jonathan@relativesanity.com>
2025-12-16 22:49:56 +0000

    removes workaround now fix is merged

--- neovim/.config/nvim/lua/plugins/colourscheme.lua
+++ neovim/.config/nvim/lua/plugins/colourscheme.lua
@@ -21,14 +21,4 @@ return {
       colorscheme = "catppuccin",
     },
   },
-  {
-    "akinsho/bufferline.nvim",
-    -- TODO: Remove this once https://github.com/LazyVim/LazyVim/pull/6354 is merged
-    init = function()
-      local bufline = require("catppuccin.groups.integrations.bufferline")
-      bufline.get = bufline.get_theme
-    end,
-    ---@module 'bufferline'
-    ---@type bufferline.Config
-  },
 }
