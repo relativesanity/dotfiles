7ecf45c0e0b7616820fcf4c423387320ff9e4463
Jonathan Barrett <jonathan@relativesanity.com>
2026-06-28 10:32:00 +0100

    toggles colourscheme via a commented alternative in init

--- neovim/.config/nvim/lua/config/init.lua
+++ neovim/.config/nvim/lua/config/init.lua
@@ -2,5 +2,5 @@ require("config.options")
 require("config.keymaps")
 require("config.lazy")
 
--- swap this line to switch themes (e.g. "catppuccin-mocha")
+-- vim.cmd.colorscheme("catppuccin-mocha")
 vim.cmd.colorscheme("base16-black-metal-bathory")
