84643115e9e3d396a274cf0756dca14ceac882ce
Jonathan Barrett <jonathan@relativesanity.com>
2026-06-29 07:22:04 +0100

    switches themes back to catppuccin mocha

--- neovim/.config/nvim/lua/config/init.lua
+++ neovim/.config/nvim/lua/config/init.lua
@@ -2,5 +2,5 @@ require("config.options")
 require("config.keymaps")
 require("config.lazy")
 
--- vim.cmd.colorscheme("catppuccin-mocha")
-vim.cmd.colorscheme("base16-black-metal-bathory")
+vim.cmd.colorscheme("catppuccin-mocha")
+-- vim.cmd.colorscheme("base16-black-metal-bathory")
