e3819503ee11746b8d26e234b59b52c80a58511c
Jonathan Barrett <jonathan@relativesanity.com>
2026-06-30 11:47:52 +0100

    adds a quiet ruby-lsp for linting and formatting

--- neovim/.config/nvim/lua/config/init.lua
+++ neovim/.config/nvim/lua/config/init.lua
@@ -2,6 +2,7 @@ require("config.options")
 require("config.keymaps")
 require("config.autocmds")
 require("config.lazy")
+require("config.lsp")
 
 vim.cmd.colorscheme("catppuccin-mocha")
 -- vim.cmd.colorscheme("base16-black-metal-bathory")
