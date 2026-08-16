e162adcfaf9f8da66fc64062c5dd3c8d5bc6fb5f
Jonathan Barrett <jonathan@relativesanity.com>
2026-08-16 13:53:53 +0100

    Returns hammerspoon to set date and time shortcuts

---
+++ hammerspoon/.hammerspoon/init.lua
@@ -0,0 +1,17 @@
+hs = hs
+
+-- Modifier key combinations
+hyper = { "ctrl", "alt", "shift", "cmd" }
+
+-- Insert today's date at the cursor, e.g. 2026-08-16
+hs.hotkey.bind(hyper, "X", function()
+	hs.eventtap.keyStrokes(os.date("%Y-%m-%d"))
+end)
+
+-- Insert the current time at the cursor, e.g. 2:32pm
+hs.hotkey.bind(hyper, "C", function()
+	local hour = tonumber(os.date("%I"))
+	local minute = os.date("%M")
+	local ampm = os.date("%p"):lower()
+	hs.eventtap.keyStrokes(string.format("%d:%s%s", hour, minute, ampm))
+end)
