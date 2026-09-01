477d11f9b5df9856c66b9bea45f5b334d0d01523
Jonathan Barrett <jonathan@relativesanity.com>
2026-09-01 21:46:07 +0100

    replaces hand-rolled neovim config with lazyvim

--- neovim/.config/nvim/lua/config/init.lua
+++
@@ -1,8 +0,0 @@
-require("config.options")
-require("config.keymaps")
-require("config.autocmds")
-require("config.lazy")
-require("config.lsp")
-
-vim.cmd.colorscheme("catppuccin-mocha")
--- vim.cmd.colorscheme("base16-black-metal-bathory")
